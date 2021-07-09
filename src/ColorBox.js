9483e3bc9e7d0df27039c4253de57119bd7d76f1
manhtran1611 <85244387+manhtran1611@users.noreply.github.com>
2021-07-09 17:44:13 +0700

    "added CopyOverLay Animation"

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,11 +2,33 @@ import React, { Component } from "react";
 import "./ColorBox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false,
+    };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+  changeCopyState(e) {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1000);
+    });
+  }
   render() {
     const { name, background } = this.props;
+    const { copied } = this.state;
     return (
-      <CopyToClipboard text={background}>
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className="ColorBox">
+          <div
+            style={{ background }}
+            className={`copy-overlay ${copied && "show"} `}
+          >
+            <div className={`copy-msg ${copied && "show"}`}>
+              <h1>copied!</h1>
+              <p>{background}</p>
+            </div>
+          </div>
           <div className="copy-container">
             <div className="box-content">
               <span>{name}</span>
