717c16672055d36695716a99960a088a41b64392
manhtran1611 <85244387+manhtran1611@users.noreply.github.com>
2021-07-09 15:15:09 +0700

    created app, added palette

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import seedColors from "./seedColors";
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navbar */}
+        <div className="Palette-colors">{/* Colors goes here */}</div>
+        {/* Footer goes here */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
---
+++ src/seedColors.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    palletteName: "Material UI Colors",
+    id: "material-ui-colors",
+    emoji: "",
+    colors: [
+      { name: "red", color: "#F44336" },
+      { name: "blue", color: "#2196F#" },
+    ],
+  },
+];
